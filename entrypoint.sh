bd74efdfc67b41379ac4ebd7ec939b5e49b97e32
onplus <31188782+onplus@users.noreply.github.com>
2019-10-08 08:38:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
 cd /v2raybin
+
 echo -e -n "$CONFIG_JSON1" > config.json
 echo -e -n "$PORT" >> config.json
 echo -e -n "$CONFIG_JSON2" >> config.json
 echo -e -n "$UUID" >> config.json
 echo -e -n "$CONFIG_JSON3" >> config.json
-if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-echo -e "$CERT_PEM" > cert.pem
-echo -e "$KEY_PEM"  > key.pem
-fi
+
 ./v2ray
